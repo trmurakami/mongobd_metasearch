b7aa4fbc5ab9cc471dc4dfbb3d3adb9c70e09ec9
Tiago Murakami <trmurakami@gmail.com>
2015-12-22 10:18:33 -0200

    Adicionado novas fontes

--- harvest_oai.sh
+++ harvest_oai.sh
@@ -1,11 +1,56 @@
 #!/bin/bash
 
-# Ciência da Informação em Revista
-catmandu import OAI --url http://www.seer.ufal.br/index.php/cir/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+# AtoZ: novas práticas em informação e conhecimento
+catmandu import OAI --url http://ojs.c3sl.ufpr.br/ojs2/index.php/atoz/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
 sleep 2
-#Revista ACB
-catmandu import OAI --url http://revista.acbsc.org.br/racb/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+# BIBLIONLINE
+catmandu import OAI --url http://periodicos.ufpb.br/ojs2/index.php/biblio/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+# Bibliotecas Universitárias: Pesquisas, experiências e perspectivas
+catmandu import OAI --url https://www.bu.ufmg.br/rbu/index.php/localhost/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+# Brazilian Journal of Information Science: Research Trends
+catmandu import OAI --url http://www2.marilia.unesp.br/revistas/index.php/bjis/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+# Ciência da Informação em Revista - CIR
+#catmandu import OAI --url http://www.seer.ufal.br/index.php/cir/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+#sleep 2
+# CRB-8 Digital
+catmandu import OAI --url http://revista.crb8.org.br/index.php/crb8digital/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+# Em Questão
+catmandu import OAI --url http://www.seer.ufrgs.br/index.php/EmQuestao/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+# Encontros Bibli: revista eletrônica de biblioteconomia e ciência da informação
+catmandu import OAI --url https://periodicos.ufsc.br/index.php/eb/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+#InCID: Revista de Ciência da Informação e Documentação
+#catmandu import OAI --url http://www.revistas.usp.br/incid/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+#sleep 2
+# Informação & Informação
+catmandu import OAI --url http://www.uel.br/revistas/uel/index.php/informacao/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+# Informação & Sociedade: Estudos
+catmandu import OAI --url http://www.ies.ufpb.br/ojs2/index.php/ies/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+# Liinc em Revista
+catmandu import OAI --url http://revista.ibict.br/liinc/index.php/liinc/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+# Perspectivas em Ciência da Informação
+catmandu import OAI --url http://portaldeperiodicos.eci.ufmg.br/index.php/pci/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+# Pesquisa Brasileira em Ciência da Informação e Biblioteconomia - PBCIB
+catmandu import OAI --url http://periodicos.ufpb.br/ojs2/index.php/pbcib/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+sleep 2
+#Revista ACB - RABC
+#catmandu import OAI --url http://revista.acbsc.org.br/racb/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+#sleep 2
+#Revista Ciência da Informação - CIInf
+#catmandu import OAI --url http://revista.ibict.br/cienciadainformacao/index.php/ciinf/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+#sleep 2
+# Tendências da Pesquisa Brasileira em Ciência da Informação
+catmandu import OAI --url http://inseer.ibict.br/ancib/index.php/tpbci/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
 sleep 2
 # Transinformação
-catmandu import OAI --url http://periodicos.puc-campinas.edu.br/seer/index.php/transinfo/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
-sleep 2
+#catmandu import OAI --url http://periodicos.puc-campinas.edu.br/seer/index.php/transinfo/oai --metadataPrefix oai_dc to MongoDB --database_name journals --bag ci
+#sleep 2
