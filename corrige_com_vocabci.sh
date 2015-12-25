d6b663284bded72429532a25c85e19a8cab324f1
Tiago Murakami <trmurakami@gmail.com>
2015-12-25 10:25:07 -0200

    Melhoria na consulta

--- corrige_com_vocabci.sh
+++ corrige_com_vocabci.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 consulta() {
-curl -s -G -L 'http://bdpife2.sibi.usp.br/vocabci/vocab/services.php?task=search&arg='$1'' | \
-xpath //string |
-sed -re 's/<\/?\w+>//g'
+curl -s -G -L 'http://bdpife2.sibi.usp.br/vocabci/vocab/services.php?task=search&arg='$1'' | xmlstarlet sel -t -v "//string"
 }
 
 while read p;
 do
  instituicao=$(cut -d "," -f 5 | sed -e 's/\"//g')
  echo -e "$instituicao\n" 
- consulta $instituicao
+consulta $instituicao
 done<$1
 
