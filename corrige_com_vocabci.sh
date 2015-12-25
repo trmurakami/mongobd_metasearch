e7c4ecc07cd2eef1ae6995895ea5cde0b59b291f
Tiago Murakami <trmurakami@gmail.com>
2015-12-25 16:52:34 -0200

    Final!

--- corrige_com_vocabci.sh
+++ corrige_com_vocabci.sh
@@ -3,17 +3,14 @@
 IFS=$'\n'       # make newlines the only separator
 for line in $(cat $1);
 do
+semmudanca=$(echo $line) 
  instituicao=$(echo "$line"|cut -d "," -f 5 | sed -e 's/\"//g')
- #printf "%s\n" "$instituicao"
  result=$(./consulta_vocabci.sh $instituicao)
 result_count=$(echo "$result" | wc -m) 
-#printf $result_count
-
  if [ $result_count -gt "1" ]
    then
-    printf "%s\n" "$result"
+    printf "%s\n" "$line" | sed "s/$instituicao/$result/g"
    else
-    printf "%s\n" "else"
+    printf "%s\n" "$line"
    fi
-#printf "%s\n" "$instituicao:" "$result"
 done 
