552356c6d247f3b1df50f4c88fd40e7c38cffdfd
Tiago Murakami <trmurakami@gmail.com>
2015-12-25 16:39:53 -0200

    Novos avanços

--- corrige_com_vocabci.sh
+++ corrige_com_vocabci.sh
@@ -6,5 +6,14 @@ do
  instituicao=$(echo "$line"|cut -d "," -f 5 | sed -e 's/\"//g')
  #printf "%s\n" "$instituicao"
  result=$(./consulta_vocabci.sh $instituicao)
- printf "%s\n" "$instituicao:" "$result"
+result_count=$(echo "$result" | wc -m) 
+#printf $result_count
+
+ if [ $result_count -gt "1" ]
+   then
+    printf "%s\n" "$result"
+   else
+    printf "%s\n" "else"
+   fi
+#printf "%s\n" "$instituicao:" "$result"
 done 
