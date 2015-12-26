45695c35140fcf9b2caa14c4ecc2ece381579247
Tiago Murakami <trmurakami@gmail.com>
2015-12-26 09:49:05 -0200

    Correção no sed para tirar o regex

--- corrige_com_vocabci.sh
+++ corrige_com_vocabci.sh
@@ -4,12 +4,12 @@ IFS=$'\n'       # make newlines the only separator
 for line in $(cat $1);
 do
 semmudanca=$(echo $line) 
- instituicao=$(echo "$line"|cut -d "," -f 5 | sed -e 's/\"//g')
+ instituicao=$(echo "$line"|cut -d "," -f 5 | sed -e 's/\"//g' )
  result=$(./consulta_vocabci.sh $instituicao)
 result_count=$(echo "$result" | wc -m) 
  if [ $result_count -gt "1" ]
    then
-    printf "%s\n" "$line" | sed "s/\"$instituicao\"/\"$result\"/g"
+    printf "%s\n" "$line" | sed "s|"$instituicao"|"$result"|g"
    else
     printf "%s\n" "$line"
    fi
