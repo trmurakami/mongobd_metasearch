edf9f3b168bb02be03032bb59044f3837245e813
Tiago Murakami <trmurakami@gmail.com>
2015-12-28 11:56:04 -0200

    Regex para tirar espaços na frente e selecionar a coluna correta

--- corrige_com_vocabci.sh
+++ corrige_com_vocabci.sh
@@ -3,10 +3,9 @@
 IFS=$'\n'       # make newlines the only separator
 for line in $(cat $1);
 do
-semmudanca=$(echo $line) 
- instituicao=$(echo "$line"|cut -d "," -f 5 | sed -e 's/\"//g' )
+ instituicao=$(echo "$line" | sed 's/\"\s/\"/g' |  sed -e 's/\",\"/\|/g' | cut -d "|" -f 4 | sed -e 's/\"//g' )
  result=$(./consulta_vocabci.sh $instituicao | sed -e '1h;2,$H;$!d;g' -e "s/).*/)/g")
-result_count=$(echo "$result" | wc -m) 
+ result_count=$(echo "$result" | wc -m) 
  if [ $result_count -gt "1" ]
    then
     printf "%s\n" "$line" | sed "s|"$instituicao"|"$result"|g"
