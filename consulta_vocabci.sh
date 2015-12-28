f86f9ec5178b1ab98cf432fe9a77fc49e29dc92e
Tiago Murakami <trmurakami@gmail.com>
2015-12-28 18:57:39 -0200

    Adição da consulta por autor

--- consulta_vocabci.sh
+++ consulta_vocabci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-query=$(php -r "echo urlencode('$1');")
+query=$(echo $1 | sed 's/ /+/g')
 url="http://bdpife2.sibi.usp.br/vocabci/vocab/services.php?task=search&arg=$query"
 result=$(curl -s -G -L $url | xmlstarlet sel -t -v "//string")
 printf "$result"
--- corrige_com_vocabci.sh
+++ corrige_com_vocabci.sh
@@ -3,13 +3,25 @@
 IFS=$'\n'       # make newlines the only separator
 for line in $(cat $1);
 do
- instituicao=$(echo "$line" | sed 's/\"\s/\"/g' |  sed -e 's/\",\"/\|/g' | cut -d "|" -f 4 | sed -e 's/\"//g' )
- result=$(./consulta_vocabci.sh $instituicao | sed -e '1h;2,$H;$!d;g' -e "s/).*/)/g")
- result_count=$(echo "$result" | wc -m) 
- if [ $result_count -gt "1" ]
+ instituicao=$(echo "$line" | sed "s/\'//g" | sed 's/\"\s/\"/g' |  sed -e 's/\",\"/\|/g' | cut -d "|" -f 4 | sed -e 's/\"//g' )
+ autor=$(echo "$line" | sed "s/\'//g" | sed 's/\"\s/\"/g' |  sed -e 's/\",\"/\|/g' | cut -d "|" -f 3 | sed -e 's/\"//g' )
+ result_inst=$(./consulta_vocabci.sh $instituicao | sed -e '1h;2,$H;$!d;g' -e "s/).*/)/g")
+ result_autor=$(./consulta_vocabci.sh $autor | sed -e '1h;2,$H;$!d;g' -e "s/).*/)/g")
+ result_count=$(echo "$result_inst" | wc -m) 
+ result_count_autor=$(echo "$result_autor" | wc -m)
+
+if [[ $result_count -gt "1" ]] 
    then
-    printf "%s\n" "$line" | sed "s|"$instituicao"|"$result"|g"
-   else
-    printf "%s\n" "$line"
-   fi
-done 
+   line=$(printf "%s\n" "$line" | sed "s|"$instituicao"|"$result_inst"|g")
+else 
+   line=$(printf "%s\n" "$line")
+fi
+if  [[ $result_count_autor -gt "1" ]]
+then
+line=$(printf "%s\n" "$line" | sed "s|"$autor"|"$result_autor"|g")
+else 
+   line=$(printf "%s\n" "$line")
+fi
+printf "%s\n" "$line"
+done
+
