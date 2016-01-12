27a977c8ff4a63f8fa73056f5e7bd9748f8605a7
Tiago Murakami <trmurakami@gmail.com>
2016-01-12 14:29:44 -0200

    Limpeza no código

--- csv_export.sh
+++ csv_export.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo mongoexport --db journals --collection ci --csv --fields _id,journalci_title,_setSpec,language,date,creator --query '{"_status" : {$not: /deleted/}}' --out export/export.csv -v
+mongoexport --db journals --collection ci --csv --fields _id,journalci_title,_setSpec,language,date,creator --query '{"_status" : {$not: /deleted/}}' --out export/export.csv -v
--- transform.sh
+++ transform.sh
@@ -20,15 +20,6 @@ consulta_termo() {
   vocabci_result=$(curl -s -G -L $url2 | xmlstarlet sel -t -v "//string")
 }
 
-
-
-#consulta_vocabci() {
-#  query=$(echo $1 | sed 's/ /+/g')
-#  url="http://bdpife2.sibi.usp.br/vocabci/vocab/services.php?task=search&arg=$query"
-#  vocabci_result=$(curl -s -G -L $url | xmlstarlet sel -t -v "//string")
-#  vocabci_result_id=$(curl -s -G -L $url | xmlstarlet sel -t -v "//term_id")
-#}
-
 consulta_instautor(){
   url="http://bdpife2.sibi.usp.br/vocabci/vocab/services.php?task=fetchRelated&arg=$1"
   instautor_result=$(curl -s -G -L "$url" | xmlstarlet sel -I -t -m '//string' -v "concat(.,'|')" | sed 's/|$//g')
@@ -49,110 +40,102 @@ date=$(printf "%s\n" "$line" | cut -d "|" -f 5 | sed 's/\"//g' | sed 's/\]//g' |
 creator=$(printf "%s\n" "$line" | cut -d "|" -f 6 | sed 's/\", \"/|/g' | sed 's/\"//g' | sed 's/\]//g' | sed 's/\[//g' | sed 's/^\s//g' | sed 's/\s$//g' )
 
 
-# | sed 's/\"//g'   
+# | sed 's/\"//g'
 
 	IFS='|' read -ra creator <<< "$creator"
 	for i in "${creator[@]}"; do
 		autor=$(echo $i | cut -d ";" -f 1 | sed 's/^\s//g' | sed 's/\s$//g')
 		autor_limpo=$(echo "$autor" | sed "s/[^a-z|0-9|A-Z| ]//g")
-		
-		if [[ $i == *";"* ]] ; 
-		
+
+		if [[ $i == *";"* ]] ;
+
 		then
-		
+
 		instituicao=$(echo $i | cut -d ";" -f 2 | sed 's/^\s//g' | sed 's/\s$//g')
-		instituicao_count=$(echo "$instituicao" | wc -m) 
-				
+		instituicao_count=$(echo "$instituicao" | wc -m)
+
 		else
-		
+
 		instituicao=$(echo $i | grep -E ';.*')
-		instituicao_count=$(echo "$instituicao" | wc -m) 
-	
-		
+		instituicao_count=$(echo "$instituicao" | wc -m)
+
+
 		fi
-		
+
 		#Consulta os autores no Vocabci usando a função consulta_vocabci
 		consulta_termo $autor_limpo
 		result_autor=$(printf "$vocabci_result" | sed -e '1h;2,$H;$!d;g' -e "s/).*/)/g")
 		result_autor_id=$(printf "$vocabci_result_id")
 		result_count_autor=$(echo "$result_autor" | wc -m)
-		
+
 			#Se autor teve resposta
 			if  [[ $result_count_autor -gt "1" ]]
 			then
 				autor_tematres=$(printf "$result_autor")
 				autor_tematres_id=$(printf "$result_autor_id")
-			else 
+			else
 			        autor_tematres=$(printf "$autor_limpo")
 				autor_tematres_id=$(printf "$autor_limpo")
 			fi
-		
-		
-			#Se tem instituição
-			
-			if [[ "$instituicao_count" -gt '1' ]]
-			then 
 
-				#IFS=',' read -ra instituicao <<< "$instituicao"
-				#for x in "${instituicao[@]}"; do
+
+			#Se tem instituição
+
+			if [[ "$instituicao_count" -gt '1' ]]
+			then
+
 					instituicao_limpa=$(echo $instituicao | sed "s/[^a-z|0-9|A-Z| ]//g" | sed 's/^\s//' | sed 's/  / /g')
-				
-				        #Consulta a instituição no Vocabci usando a função consulta_vocabci
+
+				    #Consulta a instituição no Vocabci usando a função consulta_vocabci
 					  consulta_termo $instituicao_limpa
 
-		  
+
 					  result_inst=$(printf "$vocabci_result" | head -n 1)
 					  result_inst_id=$(printf "$vocabci_result_id" | head -n 1 )
-					  result_count=$(echo "$result_inst" | wc -m) 
-				 
-			  
-	 
-						#Se instituição teve resposta
-						if [[ $result_count -gt "1" ]] 
-						then
-						   instituicao_tematres=$(printf "$result_inst") 
-						   instituicao_tematres_id=$(printf "$result_inst_id")
-						else 
-						   instituicao_tematres=$(printf "$instituicao_limpa") 
-						   instituicao_tematres_id=$(printf "$instituicao_limpa")					   
-						fi
-				
-			
-							
-					line=$(printf "%s\n" "\"$_id\",\"$journalci_title\",\"$_setSpec\",\"$language\",\"$date\",\"$autor_tematres\",\"$autor_tematres_id\",\"$instituicao_tematres\",\"$instituicao_tematres_id\"")
-				
-				
-				#done
+					  result_count=$(echo "$result_inst" | wc -m)
 
-						
+
+
+						#Se instituição teve resposta
+						if [[ $result_count -gt "1" ]]
+						then
+						   instituicao_tematres=$(printf "$result_inst")
+						   instituicao_tematres_id=$(printf "$result_inst_id")
+						else
+						   instituicao_tematres=$(printf "$instituicao_limpa")
+						   instituicao_tematres_id=$(printf "$instituicao_limpa")
+						fi
+
+
+
+					line=$(printf "%s\n" "\"$_id\",\"$journalci_title\",\"$_setSpec\",\"$language\",\"$date\",\"$autor_tematres\",\"$autor_tematres_id\",\"$instituicao_tematres\",\"$instituicao_tematres_id\"")
+
+
 			else
 			   	  line=$(printf "%s\n" "\"$_id\",\"$journalci_title\",\"$language\",\"$date\",\"$autor_tematres\",\"$autor_tematres_id\",\"\",\"\"")
 
 
-					#Verifica se o autor tem ID e consulta no 
-				
-				
+					#Verifica se o autor tem ID e consulta no
+
+
 				        autor_tematres_id_numero=$(printf "$autor_tematres_id" | sed 's/[^0-9]//g')
-					
-				
+
+
 					if  [[ $autor_tematres_id_numero -eq NULL ]]
 					then
-						line=$(printf "%s\n" "\"$_id\",\"$journalci_title\",\"$_setSpec\",\"$language\",\"$date\",\"$autor_tematres\",\"$autor_tematres_id\",\"\",\"\"") 
-   				    
+						line=$(printf "%s\n" "\"$_id\",\"$journalci_title\",\"$_setSpec\",\"$language\",\"$date\",\"$autor_tematres\",\"$autor_tematres_id\",\"\",\"\"")
+
 					else
 					    consulta_instautor $result_autor_id
-					    instituicao_tematres=$(printf "$instautor_result") 
+					    instituicao_tematres=$(printf "$instautor_result")
 					    instituicao_tematres_id=$(printf "$instautor_result_id")
-					    line=$(printf "%s\n" "\"$_id\",\"$journalci_title\",\"$_setSpec\",\"$language\",\"$date\",\"$autor_tematres\",\"$autor_tematres_id\",\"$instituicao_tematres\",\"$instituicao_tematres_id\"") 						
-				    
-					fi 
+					    line=$(printf "%s\n" "\"$_id\",\"$journalci_title\",\"$_setSpec\",\"$language\",\"$date\",\"$autor_tematres\",\"$autor_tematres_id\",\"$instituicao_tematres\",\"$instituicao_tematres_id\"")
 
-
+					fi
 
 			fi
-			
-		printf "%s\n" "$line" >> $2		
+
+		printf "%s\n" "$line" >> $2
 	done
 
 done
-
