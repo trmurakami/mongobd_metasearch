c9c145c968e47a3032a291a543761e406b8168b2
Tiago Murakami <trmurakami@gmail.com>
2016-01-24 01:48:12 -0200

    Alteração simples

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,11 +1,10 @@
 <!-- Cabeçalho -->
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-12">
       <div class="page-header">
         <h1><a href="index.php">MetaBuscaCI</a> <small style="font-size: 20px">Metabuscador em periódicos de Ciência da Informação</small></h1>
       </div>
     </div>
-    <div class="col-md-4"></div>
   </div>
 
 <!-- Barra de navegação -->
