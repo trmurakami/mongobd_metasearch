f54303377360f3c3d52dc5b028f034bebd28afee
Tiago Murakami <trmurakami@gmail.com>
2016-01-24 01:07:22 -0200

    Criado a rotina para rodar os autores e instituicoes

---
+++ sh/run_autor_e_instituicao.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm ../export/export.csv
+sleep 2
+mongoexport --db journals --collection ci --type=csv --fields _id,creator --out ../export/export.csv -v
+sleep 2
+echo 'db.ci.update({},{$unset: {autor:1}},false,true)' | mongo journals
+echo 'db.ci.update({},{$unset: {instituicao:1}},false,true)' | mongo journals
+sleep 2
+./transform.sh ../export/export.csv ../export/export_final.csv
