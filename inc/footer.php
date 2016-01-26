07f059af3dfed66b33f4b2924704d09471d6fa0f
Tiago Murakami <trmurakami@gmail.com>
2016-01-26 14:14:18 -0200

    Correção do link

--- inc/footer.php
+++ inc/footer.php
@@ -3,6 +3,6 @@
 <div class="row">
   <div class="col-md-8"><p>MetaBuscaCi foi desenvolvido usando o software livre <a href="https://github.com/trmurakami/mongobd_metasearch">MongoDB-MetaSearch</p></a></div>
   <div class="col-md-4"><p>
-    <div class="fb-like" data-href="https://bdpife2.sibi.usp.br/metabuscaci" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+    <div class="fb-like" data-href="http://bdpife2.sibi.usp.br/metabuscaci" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
   </div></p></div>
 </div>
