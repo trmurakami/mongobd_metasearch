a011fc05ca9a4006ab92723c60917baa5ed0f9eb
Tiago Murakami <trmurakami@gmail.com>
2016-01-28 07:30:34 -0200

    Mudanças na página sobre

--- about.php
+++ about.php
@@ -13,7 +13,7 @@
   <div class="row">
     <div class="col-md-12">
       <p>O MetaBuscaCI é uma metabusca em periódicos da Ciência da Informação brasileiros que disponibilizam seus dados por meio de OAI-PMH.</p>
-      <p>Está sendo desenvolvido por Tiago Rodrigo Marçal Murakami, do Sistema Integrado de Bibliotecas da Universidade de São Paulo, em parceria com o Prof. Ronaldo Ferreira de Araújo, da Universidade Federal de Alagoas</p>
+      <p>Está sendo desenvolvido por <b>Tiago Rodrigo Marçal Murakami ( tiago.murakami EM dt.sibi.usp.br )</b>, do Sistema Integrado de Bibliotecas da Universidade de São Paulo, em parceria com o <b>Prof. Ronaldo Ferreira de Araújo ( ronaldfa EM gmail.com )</b>, da Universidade Federal de Alagoas</p>
 
       <p>Creditos da imagem: https://c2.staticflickr.com/2/1413/608563522_474c1d277e_b.jpg</p>
     </div>
