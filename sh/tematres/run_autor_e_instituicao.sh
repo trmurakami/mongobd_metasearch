51fdf0084c442c0296a91a28f5dfc2615e153393
Tiago Murakami <trmurakami@gmail.com>
2016-02-19 13:31:08 -0200

    Correção no script

--- sh/tematres/run_autor_e_instituicao.sh
+++ sh/tematres/run_autor_e_instituicao.sh
@@ -2,9 +2,9 @@
 
 rm ../export/export.csv
 sleep 2
-mongoexport --db journals --collection ci --type=csv --fields _id,creator --out ../export/export.csv -v
+mongoexport --db journals --collection ci --type=csv --fields _id,creator --out ../../export/export.csv -v
 sleep 2
 echo 'db.ci.update({},{$unset: {autor:1}},false,true)' | mongo journals
 echo 'db.ci.update({},{$unset: {instituicao:1}},false,true)' | mongo journals
 sleep 2
-./transform.sh ../export/export.csv ../export/export_final.csv
+./transform.sh ../../export/export.csv ../../export/export_final.csv
