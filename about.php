2ba2ddbc7288f572aaf4901f33eeedeba4f32988
Tiago Murakami <trmurakami@gmail.com>
2016-02-26 08:26:20 -0300

    Alteração do endereço do lab-imetrics

--- about.php
+++ about.php
@@ -13,9 +13,8 @@
   <div class="row">
     <div class="col-md-12">
       <p>O MetaBuscaCI é uma metabusca em periódicos da Ciência da Informação brasileiros que disponibilizam seus dados por meio de OAI-PMH.</p>
-      <p>Está sendo desenvolvido no <a href="https://ronaldfar.wordpress.com/lab-imetrics/">Laboratório de Estudos Métricos da Informação na Web (Lab-iMetrics)</a> na linha de pesquisa Ciência 2.0 e os aportes da altmetria por <b>Tiago Rodrigo Marçal Murakami</b> do Sistema Integrado de Bibliotecas da Universidade de São Paulo, em parceria com o <b>Prof. Dr. Ronaldo Ferreira de Araujo</b> do Curso de BIblioteconomia do Instituto de Ciências Humanas, Comunicação e Artes da Universidade Federal de Alagoas (CB/ICHCA/UFAL)</p>
+      <p>Está sendo desenvolvido no <a href="http://dgp.cnpq.br/dgp/espelhogrupo/5207019450670905">Laboratório de Estudos Métricos da Informação na Web (Lab-iMetrics)</a> na linha de pesquisa Ciência 2.0 e os aportes da altmetria por <b>Tiago Rodrigo Marçal Murakami</b> do Sistema Integrado de Bibliotecas da Universidade de São Paulo, em parceria com o <b>Prof. Dr. Ronaldo Ferreira de Araujo</b> do Curso de BIblioteconomia do Instituto de Ciências Humanas, Comunicação e Artes da Universidade Federal de Alagoas (CB/ICHCA/UFAL)</p>
 
-      <p><a href="https://c2.staticflickr.com/2/1413/608563522_474c1d277e_b.jpg">Creditos da imagem</a></p>
     </div>
  </div>
 </div>
--- inc/footer.php
+++ inc/footer.php
@@ -3,25 +3,21 @@
   <div class="ui container">
     <div class="ui stackable inverted divided equal height stackable grid">
       <div class="three wide column">
-        <h4 class="ui inverted header">About</h4>
+        <h4 class="ui inverted header">Sobre</h4>
         <div class="ui inverted link list">
+          <a href="about.php" class="item">Sobre nós</a>
           <a href="#" class="item">Sitemap</a>
-          <a href="#" class="item">Contact Us</a>
-          <a href="#" class="item">Religious Ceremonies</a>
-          <a href="#" class="item">Gazebo Plans</a>
+          <a href="contact.php" class="item">Entre em contato</a>
         </div>
       </div>
       <div class="three wide column">
-        <h4 class="ui inverted header">Services</h4>
+        <h4 class="ui inverted header">Dados</h4>
         <div class="ui inverted link list">
-          <a href="#" class="item">Banana Pre-Order</a>
-          <a href="#" class="item">DNA FAQ</a>
-          <a href="#" class="item">How To Access</a>
-          <a href="#" class="item">Favorite X-Men</a>
+          <a href="statistics.php" class="item">Estatísticas</a>
         </div>
       </div>
       <div class="seven wide column">
-        <h4 class="ui inverted header"><a href="https://ronaldfar.wordpress.com/lab-imetrics/">Laboratório de Estudos Métricos da Informação na Web (Lab-iMetrics)</a></h4>
+        <h4 class="ui inverted header"><a href="http://dgp.cnpq.br/dgp/espelhogrupo/5207019450670905">Laboratório de Estudos Métricos da Informação na Web (Lab-iMetrics)</a></h4>
         <p><?php echo gettext("branch");?> foi desenvolvido usando o software livre <a href="https://github.com/trmurakami/mongobd_metasearch">MongoDB-MetaSearch</a></p>
         <div class="fb-like" data-href="http://bdpife2.sibi.usp.br/metabuscaci" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
       </div>
