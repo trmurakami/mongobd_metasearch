deabc103dd05e6cab74db2ccdb12867c930517d6
Tiago Murakami <trmurakami@gmail.com>
2016-02-28 19:49:50 -0300

    Correção no sobre

--- about.php
+++ about.php
@@ -12,7 +12,7 @@
 <div class="ui container">
   <div class="row">
     <div class="col-md-12">
-      <p>O MetaBuscaCI é uma metabusca em periódicos da Ciência da Informação brasileiros que disponibilizam seus dados por meio de OAI-PMH.</p>
+      <p>O Repertório da Produção Periódica Brasileira de Ciência da Informação (RPPBCI) é um coletânea de metadados de artigos de periódicos de Ciência da Informação disponibilizados em OAI-PMH.</p>
       <p>Está sendo desenvolvido no <a href="http://dgp.cnpq.br/dgp/espelhogrupo/5207019450670905">Laboratório de Estudos Métricos da Informação na Web (Lab-iMetrics)</a> na linha de pesquisa Ciência 2.0 e os aportes da altmetria por <b>Tiago Rodrigo Marçal Murakami</b> do Sistema Integrado de Bibliotecas da Universidade de São Paulo, em parceria com o <b>Prof. Dr. Ronaldo Ferreira de Araujo</b> do Curso de BIblioteconomia do Instituto de Ciências Humanas, Comunicação e Artes da Universidade Federal de Alagoas (CB/ICHCA/UFAL)</p>
 
     </div>
