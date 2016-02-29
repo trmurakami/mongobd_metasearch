72646291e3f13d054466b9440e1812d44f2b7c90
Tiago Murakami <trmurakami@gmail.com>
2016-02-29 16:11:45 -0300

    Update para assunto

--- sh/tematres/run_subject_update.sh
+++ sh/tematres/run_subject_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm ../export/subject_update.csv
-rm ../export/subject_corrigido_update.csv
+rm ../../export/subject_update.csv
+rm ../../export/subject_corrigido_update.csv
 sleep 2
-mongoexport --db journals --collection ci --type=csv --fields _id,subject --query '{ "assunto_tematres" : { "$exists" : false } }' --out ../export/subject_update.csv
+mongoexport --db journals --collection ci --type=csv --fields _id,subject --query '{ "assunto_tematres" : { "$exists" : false } }' --out ../../export/subject_update.csv
 sleep 2
-./transform_assunto.sh ../export/subject_update.csv ../export/subject_corrigido_update.csv
+./transform_assunto.sh ../../export/subject_update.csv ../../export/subject_corrigido_update.csv
