fec3ff7dd6fbf9c250ee7195217fdfeb4aee8c87
Tiago Murakami <trmurakami@gmail.com>
2016-03-02 13:07:14 -0300

    Novas correções

--- sh/altmetrics/run_altmetrics_update.sh
+++ sh/altmetrics/run_altmetrics_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm ../../export/identifier_update.csv
-mongoexport --db journals --collection ci --type=csv --fields _id,identifier --query '{ "facebook_atualizacao" : "00000000"}' --out ../export/identifier_update.csv
+mongoexport --db journals --collection ci --type=csv --fields _id,identifier --query '{ "facebook_atualizacao" : "00000000"}' --out ../../export/identifier_update.csv
 ./altmetrics_facebook.sh ../../export/identifier_update.csv
 sleep 2
 rm ../../export/relation_update.csv
-mongoexport --db journals --collection ci --type=csv --fields relation --query '{ "facebook_atualizacao" : "00000000"}' --out ../export/relation.csv
-./altmetrics_facebook_relation.sh ../../export/relation.csv
+mongoexport --db journals --collection ci --type=csv --fields relation --query '{ "facebook_atualizacao" : "00000000"}' --out ../../export/relation.csv
+./altmetrics_facebook_relation.sh ../../export/relation_update.csv
