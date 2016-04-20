e3f2236237043963edce785a689ecd88fb7d0d7c
Tiago Murakami <trmurakami@gmail.com>
2016-04-20 04:53:31 -0300

    Paginação

--- result.php
+++ result.php
@@ -260,12 +260,12 @@ if ($q1 == "altmetrics.references") {
 
 <?php
 /* Pagination - Start */
-echo 'Página'.$page.'de'.ceil($total / $limit).'';
 echo '<br/><div class="ui buttons">';
 if($page > 1){
   echo '<form method="post" action="'.$escaped_url.'">';
   echo '<input type="hidden" name="extra_submit_param" value="extra_submit_value">';
   echo '<button type="submit" name="page" class="ui labeled icon button active" value="'.$prev.'"><i class="left chevron icon"></i>Anterior</button>';
+  echo '<button class="ui button">'.$page.' de '.ceil($total / $limit).'</button>';
   if($page * $limit < $total) {
     echo '<button type="submit" name="page" value="'.$next.'" class="ui right labeled icon button active">Próximo<i class="right chevron icon"></i></button>';
   }
@@ -278,6 +278,7 @@ if($page > 1){
       echo '<form method="post" action="'.$escaped_url.'">';
       echo '<input type="hidden" name="extra_submit_param" value="extra_submit_value">';
       echo '<button class="ui labeled icon button disabled"><i class="left chevron icon"></i>Anterior</button>';
+      echo '<button class="ui button">'.$page.' de '.ceil($total / $limit).'</button>';
       echo '<button type="submit" name="page" value="'.$next.'" class="ui right labeled icon button active">Próximo<i class="right chevron icon"></i></button>';
       echo '</form>';
     }
