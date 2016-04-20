16cb6fa6c0e4ae17b2034af62893dafe3931a8a7
Tiago Murakami <trmurakami@gmail.com>
2016-04-19 21:29:22 -0300

    References nas estatísticas

--- result.php
+++ result.php
@@ -17,7 +17,7 @@ if (empty($_GET)) {
         foreach ($_GET as $key => $value) {
           $consult .= '"'.$key.'":"'.$value.'",';
         }
-        $query = json_decode('{'.$consult.'"$altmetrics.references": {"$regex":"'.$q.'"}}');
+        $query = json_decode('{'.$consult.'"altmetrics.references": {"$regex":"'.$q.'", "$options": "si"}}');
 } elseif (!empty($_GET["category"])) {
     unset ($_GET["category"]);
     $q = str_replace('"','\\"',$_GET["q"]);
